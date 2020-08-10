ce872dbfc9e2f91f62962c727652413d82458437
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2020-08-10 15:05:04 -0700

    added eslint

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const wasm_module_promise = import('./pkg');
-import vertex_path from './assets/tetra.vert';
-import frag_path from './assets/tetra.frag';
+const wasmModulePromise = import('./pkg');
+import vertexPath from './assets/tetra.vert';
+import fragPath from './assets/tetra.frag';
 
+/**
+ * Main function that handles webgl instantiation and linking with wasm
+ */
 async function main() {
-    const wasm_module = await wasm_module_promise;
-    const { Tetra } = wasm_module;
-    const vertex_source = await (await fetch(vertex_path)).text();
-    const frag_source = await (await fetch(frag_path)).text();
-    new Tetra(document.getElementById('webgl'))
-        .add_vert_shader(vertex_source)
-        .add_frag_shader(frag_source)
-        .link_program()
-        .add_vertices([
-            -0.7, -0.7, 0.0, // bottom left
-            0.7, -0.7, 0.0, // bottom right
-            0.0, 0.7, 0.0, // top
-        ])
-        .draw();
+  const wasmModule = await wasmModulePromise;
+  const {Tetra} = wasmModule;
+  const [vertexSource, fragSource] = await Promise.all(
+      [vertexPath, fragPath].map(async (path) => {
+        return await (await fetch(path)).text();
+      }));
+  new Tetra(document.getElementById('webgl'))
+      .add_vert_shader(vertexSource)
+      .add_frag_shader(fragSource)
+      .link_program()
+      .add_vertices([
+        -0.7, -0.7, 0.0, // bottom left
+        0.7, -0.7, 0.0, // bottom right
+        0.0, 0.7, 0.0, // top
+      ])
+      .draw();
 }
 
 main();
